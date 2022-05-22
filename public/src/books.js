78f41a0bb0ba67f7c8ec0d83f1eab8019f338d21
Zachary Jordan <zacjor1@gmail.com>
2022-05-22 15:33:00 -0500

    removed superfluous parentheses

--- public/src/books.js
+++ public/src/books.js
@@ -1,11 +1,11 @@
-const findAuthorById = (authors, id) => authors.find((author) => author.id === id);
-const findBookById = (books, id) => books.find((book) => book.id === id);
+const findAuthorById = (authors, id) => authors.find(author => author.id === id);
+const findBookById = (books, id) => books.find(book => book.id === id);
 
 // The books array is filtered into two seperate arrays based on returned status.
 // The two arrays are returned in a single array.
 function partitionBooksByBorrowedStatus(books) {
-  const booksOut = books.filter((book)=> book.borrows[0].returned == false);
-  const booksIn = books.filter((book) => book.borrows[0].returned == true);
+  const booksOut = books.filter(book=> book.borrows[0].returned == false);
+  const booksIn = books.filter(book => book.borrows[0].returned == true);
   return [booksOut, booksIn];
 }
 
@@ -14,7 +14,7 @@ function partitionBooksByBorrowedStatus(books) {
 // inside the book object. A "returned: value" key is added to each object in
 //the "matched" array. The "borrowerList" is returned sliced.
 function getBorrowersForBook(book, accounts) {
-  const borrowerList = book.borrows.map((borrower) => {
+  const borrowerList = book.borrows.map(borrower => {
     const matched = accounts.find(account => borrower.id === account.id)
     matched.returned = borrower.returned;
     return matched;
